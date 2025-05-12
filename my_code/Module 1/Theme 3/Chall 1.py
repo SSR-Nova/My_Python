4c5f7820ec35cc28e417958f9bd5ae0170473168
SSR-Nova <faustpoletaev1@rambler.ru>
2025-05-12 22:11:05 +0400

    изменени названия

--- my_code/Module 1/Theme 3/Chall 1.py
+++ my_code/Module 1/Theme 3/Chall 1.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 yars = int(input('Введите год своего рождения: '))
 age = 2025 - yars
 is_adult = age >= 18
-=======
-yars = int(input('Введите год своего рождения: '))
-age = 2025 - yars
-is_adult = age >= 18
->>>>>>> d6adf397ce76f5063ab9da53365e45c33c9cf186
 print(f'Вам {age} лет. Совершеннолетний: {is_adult}')
